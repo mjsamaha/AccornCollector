96e1d5177d91af3ae3cd73ebf8438fb6fe6deb0f
mjsamaha <matthewsamaha@gmail.com>
2025-08-24 23:03:50 -0400

    updated GameScreen.java

--- core/src/main/java/com/lobsterchops/accorncollector/screens/GameScreen.java
+++ core/src/main/java/com/lobsterchops/accorncollector/screens/GameScreen.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
@@ -18,34 +17,34 @@ import com.lobsterchops.accorncollector.Player;
 
 public class GameScreen implements Screen {
 	private ScreenManager screenManager;
-	
-	
+
+
 	private FitViewport viewport;
-	
-	
+
+
 	/* rendering */
 	private SpriteBatch batch;
 	private BitmapFont font;
 	private GlyphLayout layout;
-	
-	
+
+
 	/* assets */
 	private Texture backgroundTexture;
 	private Texture playerTexture;
 	private Texture acornTexture; // note: original file name can remain "accorn.png"
 	private Sound collectSound;
 	private Music gameplayMusic;
-	
-	
+
+
 	/* game objects */
 	private Player player;
 	private AccornManager accornManager;
-	
-	
+
+
 	public GameScreen(ScreenManager screenManager) {
 		this.screenManager = screenManager;
 	}
-	
+
 	@Override
 	public void show() {
 		setupRendering();
@@ -54,7 +53,7 @@ public class GameScreen implements Screen {
 		createGameObjects();
 		setupAudio();
 	}
-	
+
 	private void setupRendering() {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
@@ -62,11 +61,11 @@ public class GameScreen implements Screen {
 		font.getData().setScale(1.5f);
 		font.setColor(Color.WHITE);
 	}
-	
+
 	private void setupViewport() {
 		viewport = new FitViewport(8, 6);
 	}
-	
+
 	private void loadAssets() {
 		backgroundTexture = new Texture("background.png");
 		playerTexture = new Texture("player.png");
@@ -76,7 +75,7 @@ public class GameScreen implements Screen {
 		collectSound = Gdx.audio.newSound(Gdx.files.internal("collect.wav"));
 		gameplayMusic = Gdx.audio.newMusic(Gdx.files.internal("Amber Forest.mp3"));
 	}
-	
+
 	private void createGameObjects() {
 		// place player at bottom center
 		float startX = viewport.getWorldWidth() / 2f - 0.5f;
@@ -88,26 +87,26 @@ public class GameScreen implements Screen {
 		// create the first acorn using the viewport so it can spawn at the top of the world
 		accornManager.createAccorn(viewport);
 	}
-	
+
 	private void setupAudio() {
 		gameplayMusic.setLooping(true);
 		gameplayMusic.setVolume(.5f);
 		gameplayMusic.play();
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		handleInput();
 		update(delta);
 		draw();
 	}
-	
+
 	private void handleInput() {
 		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
 			screenManager.setScreen(new MainMenuScreen(screenManager), ScreenManager.TransitionType.FADE, 0.5f);
 		}
 	}
-	
+
 	private void update(float delta) {
 		// update player (handles its own input + bounds)
 		player.update(delta, viewport);
@@ -116,7 +115,7 @@ public class GameScreen implements Screen {
 		// update acorns and check collisions
 		accornManager.update(delta, viewport, player.getBounds());
 	}
-	
+
 	private void draw() {
 		ScreenUtils.clear(Color.BLACK);
 		viewport.apply();
@@ -144,40 +143,53 @@ public class GameScreen implements Screen {
 	public void resize(int width, int height) {
 		viewport.update(width, height, true);
 	}
-	
+
 	@Override
 	public void pause() {
-		
+
 	}
 
 
 	@Override
 	public void resume() {
-		
+
 	}
 
 
 	@Override
 	public void hide() {
-		
+
 	}
-	
+
 	@Override
 	public void dispose() {
 		// dispose everything this screen created
-		if (batch != null) batch.dispose();
-		if (font != null) font.dispose();
-	
-	
-		if (backgroundTexture != null) backgroundTexture.dispose();
-		if (playerTexture != null) playerTexture.dispose();
-		if (acornTexture != null) acornTexture.dispose();
-	
-	
-		if (collectSound != null) collectSound.dispose();
-		if (gameplayMusic != null) gameplayMusic.dispose();
+		if (batch != null) {
+			batch.dispose();
+		}
+		if (font != null) {
+			font.dispose();
+		}
+
+
+		if (backgroundTexture != null) {
+			backgroundTexture.dispose();
+		}
+		if (playerTexture != null) {
+			playerTexture.dispose();
+		}
+		if (acornTexture != null) {
+			acornTexture.dispose();
+		}
+
+
+		if (collectSound != null) {
+			collectSound.dispose();
+		}
+		if (gameplayMusic != null) {
+			gameplayMusic.dispose();
+		}
 
 	// player and acornManager do not own textures/sounds so no double-dispose
 	}
 }
-	
